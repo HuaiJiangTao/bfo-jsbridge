ab41e95c5e8054cadfe63fa94c2eb83f88c53591
槐江涛 <huaijiangtao@sogou-inc.com>
2019-09-09 14:25:26 +0800

    [项目生成] A 添加代码文件

---
+++ gruntfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = function (grunt) {
+    require("load-grunt-tasks")(grunt);
+    grunt.initConfig({
+        clean: ['build'],
+        uglify: {
+            options: {
+                banner: '/* Build Time: <%= grunt.template.today("yyyy-mm-dd H:MM:ss") %> */'
+            },
+            jsbridge: {
+                files: {
+                    'dist/index.js': ['src/index.js']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('build', ['clean', 'uglify']);
+}
